a28311a6adc7ac7b089a0bc6f9e9e12b0a3ffebb
Sahmey <sahmeykhan88@gmail.com>
2025-06-15 18:21:22 +0600

    Product List

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,7 @@ import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -274,8 +274,28 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+{plantsArray.map((category,index) => (
+    <div key={index}>
+<h1>
+    <div className='plant_heading' >{category.category}</div>
+</h1>
+<div className='product-list'> 
+    {category.plants.map((plant, plantIndex) => (
+        <div className='product-card' key={index}>
+          <img className='product-image' src={plant.image} alt={plant.name}/>
+          <div className='product-title'>{plant.name}</div>
+          <div className='product-description'>{plant.description}</div>
+          <div className='product-price'>{plant.cost}</div>
+          <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+
+            </div>
+    ))}
+</div>
 
 
+
+        </div>
+))}
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
